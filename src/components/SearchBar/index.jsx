69dbd473245e8ed01af00c6d70c1fc609ae6b2ae
Danton Matheus <dantoncosta03@hotmail.com>
2022-08-15 18:29:54 -0300

    :memo: Cria a feat da sidebar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,19 +1,23 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import {DebounceInput} from 'react-debounce-input';
+import { useNavigate } from 'react-router-dom';
 import { getApiUrl } from '../../utils/apiUtils';
-import { InputBar, SearchContainer } from './styles';
+import { InputBar, SearchContainer, SearchResult, SearchResultsPanel } from './styles';
 
 export default function SearchBar() {
   const [search, setSearch] = useState('');
   const [usersData, setUsersData] = useState([]);
+  const navigate = useNavigate();
 
   const filteredSearch = search.length >= 3 ? 
     usersData.filter(user => {
-      user.username.toLowerCase().includes(search.toLowerCase());
+      return user.username.toLowerCase().includes(search.toLowerCase());
     }) 
   : [];
-  
+
+  console.log("filteredSearch no início")
+  console.log(filteredSearch)
 
   useEffect(() => {
     const API_URL = getApiUrl("users");
@@ -26,6 +30,41 @@ export default function SearchBar() {
     });
   }, []);
 
+  console.log("usersData")
+  console.log(usersData)
+
+  function goToUserPage(id) {
+    navigate(`/user/${id}`)
+  }
+
+  function renderResults() {
+    console.log("filteredSearch");
+    console.log(filteredSearch);
+    if (search.length >= 3) {
+      return(
+        filteredSearch.length > 0?
+        <SearchResultsPanel>
+          {filteredSearch.map((user, index) => {
+              return(
+                <SearchResult key={index} onClick={() => goToUserPage(user.id)}>
+                  <img src={user.picture} alt="profile image" />
+                  {user.username}
+                </SearchResult>
+              )
+            })
+          }
+        </SearchResultsPanel> 
+        : 
+        <SearchResultsPanel>
+          <SearchResult>
+            Nenhum usuário encontrado
+          </SearchResult>
+        </SearchResultsPanel>
+      ); 
+    }
+    return
+  }
+
   return(
     <SearchContainer>
       <DebounceInput
@@ -37,6 +76,11 @@ export default function SearchBar() {
         value={search}
         onChange={e => setSearch(e.target.value)}
       />
+      {usersData ? 
+          renderResults()
+        : 
+          ''
+      }
     </SearchContainer>
   );
 }
--- src/components/SearchBar/styles.js
+++ src/components/SearchBar/styles.js
@@ -19,6 +19,7 @@ const InputBar = styled.input`
 
 const SearchContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   width: 40%;
   max-width: 563px;
@@ -27,4 +28,47 @@ const SearchContainer = styled.div`
   background-color: transparent;
 `;
 
-export {InputBar, SearchContainer}
+const SearchResultsPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+  width: 100%;
+  padding: 8px;
+  overflow-y: scroll;
+  max-width: 563px;
+  height: auto;
+  max-height: 131px;
+  background-color:  #E7E7E7;
+`;
+
+const SearchResult = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  gap: 12px;
+  padding-left: 4px;
+  background-color:  #E7E7E7;
+
+  font-family: 'Lato';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 19px;
+  line-height: 23px;
+  color: #515151;
+
+  img{
+    width: 39px;
+    height: 39px;
+    border-radius: 100%;
+    object-fit: cover;
+  }
+
+  &:hover{
+    cursor: pointer;
+    filter: brightness(0.6);
+  }
+`;
+
+export {InputBar, SearchContainer, SearchResultsPanel, SearchResult}
