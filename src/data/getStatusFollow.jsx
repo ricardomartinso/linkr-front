c00dadc327665e8d457d56f7cb776a72a4252794
josimar120 <josimar.17perseu@gmail.com>
2022-08-19 14:14:31 -0300

    refactor:refatorando o status do seguidor

--- src/data/getStatusFollow.jsx
+++ src/data/getStatusFollow.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { getApiUrl, getConfig } from "../utils/apiUtils";
 
 export async function getStatusFollow(id, token){
-    console.log("OOOOOOOOOOOOOO",token)
     try {
         const resp = await axios.get(getApiUrl(`user/follower/status/${id}`),getConfig(token));
         return  resp;
